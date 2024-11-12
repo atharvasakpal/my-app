4b9e830e17a2d9725c02caf44fece02cafbceb48
Atharva Sakpal <atharvasakpal14@gmail.com>
2024-11-12 20:18:42 +0530

    first commit

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "../globals.css";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        
+        {children}
+    </main>
+  );
+}
